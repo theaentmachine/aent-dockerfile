ac4538485b85d4ef6dd22ff5f2068006760f9590
Jindun SHAO <sjindun@gmail.com>
2018-06-28 12:09:10 +0200

    deleting Add command

--- src/aent.php
+++ src/aent.php
@@ -4,12 +4,10 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use TheAentMachine\AentApplication;
-use TheAentMachine\AentDockerfile\Command\AddEventCommand;
 use TheAentMachine\AentDockerfile\Command\NewImageEventCommand;
 
 $application = new AentApplication();
 
-$application->add(new AddEventCommand());
 $application->add(new NewImageEventCommand());
 
 $application->run();
